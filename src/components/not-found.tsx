6088e078fb7c6776cde5c295d0507d28022918fc
Valentín Vera <valentinvera2805@gmail.com>
2025-02-13 13:46:32 -0300

    feat: add component not found for error 404

---
+++ src/components/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="p-4">
+      <h1 className="font-bold text-2xl">404 Not Found</h1>
+      <p>La página que buscas no existe.</p>
+    </div>
+  )
+}
+
+export default NotFound
