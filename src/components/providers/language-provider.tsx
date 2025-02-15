be649940d22f66e4bd6e637a8b9caafe1afaff88
Valentín Vera <valentinvera2805@gmail.com>
2025-02-15 12:02:57 -0300

    feat: add provider for language

---
+++ src/components/providers/language-provider.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useEffect, useState } from "react"
+import { useTranslation } from "react-i18next" // Importa useTranslation
+
+interface LanguageContextType {
+  language: string
+  toggleLanguage: () => void
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(undefined)
+
+export const LanguageProvider = ({ children }: { children: React.ReactNode }) => {
+  const [language, setLanguage] = useState<string>(() => {
+    return localStorage.getItem("language") || "es"
+  })
+
+  const { i18n } = useTranslation()
+
+  const toggleLanguage = () => {
+    const newLang = language === "es" ? "en" : "es"
+    setLanguage(newLang)
+    localStorage.setItem("language", newLang)
+  }
+
+  useEffect(() => {
+    i18n.changeLanguage(language)
+  }, [language, i18n])
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  )
+}
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext)
+
+  if (!context) {
+    throw new Error("useLanguage must be used within a LanguageProvider")
+  }
+
+  return context
+}
