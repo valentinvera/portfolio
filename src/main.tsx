4e92b083d5280b4713b812969e51a89fb2f6f242
Valentín Vera <valentinvera2805@gmail.com>
2025-02-15 12:05:25 -0300

    feat: add language provider that wraps the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./styles/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/providers/theme-provider"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { NotFoundRoute, RouterProvider, createRouter } from "@tanstack/react-router"
 import NotFound from "./components/not-found"
 import { routeTree } from "./routeTree.gen"
 import "./i18n"
+import { LanguageProvider } from "@/components/providers/language-provider"
 
 const notFoundRoute = new NotFoundRoute({
   getParentRoute: () => routeTree,
@@ -28,9 +29,11 @@ declare module "@tanstack/react-router" {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <TooltipProvider delayDuration={0}>
-        <RouterProvider router={router} />
-      </TooltipProvider>
+      <LanguageProvider>
+        <TooltipProvider delayDuration={0}>
+          <RouterProvider router={router} />
+        </TooltipProvider>
+      </LanguageProvider>
     </ThemeProvider>
   </StrictMode>,
 )
