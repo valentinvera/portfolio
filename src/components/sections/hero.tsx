a1246e244b64571ae02de65b3c0dcfeead95ad8d
Valentín Vera <valentinvera2805@gmail.com>
2025-02-15 11:50:23 -0300

    fix: change the p tag to div due to DOM nesting validation issues and add animations for an icon

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -15,14 +15,14 @@ const Hero = () => {
         <h1 className="pr-4 font-bold ml:text-5xl text-4xl leading-tight sm:pr-0 sm:leading-none">
           {t("main.hero.title")}
         </h1>
-        <p className="ml:-mb-3 ml:w-[80%] ml:text-xl text-muted-foreground sm:w-[65%]">
+        <div className="ml:-mb-3 ml:w-[80%] ml:text-xl text-muted-foreground sm:w-[65%]">
           {t("main.hero.subtitle")}
           <RotatingText
             texts={rotatingTexts}
             mainClassName="inline-flex dark:bg-white bg-[hsl(0-0%-98%)] px-1 ml-2 rounded-sm"
             elementLevelClassName="dark:text-black text-white font-bold"
           />
-        </p>
+        </div>
         <StatusWork isAvailable={true} />
         <div className="flex gap-4">
           <a
@@ -35,10 +35,13 @@ const Hero = () => {
           </a>
           <a
             href="#contact"
-            className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full bg-primary ml:px-8 px-6 ml:py-4 py-4 font-medium ml:text-base text-primary-foreground text-sm transition-colors hover:bg-primary/90 disabled:pointer-events-none disabled:opacity-50"
+            className="group inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full bg-primary ml:px-8 px-6 ml:py-4 py-4 font-medium ml:text-base text-primary-foreground text-sm transition-colors hover:bg-primary/90 disabled:pointer-events-none disabled:opacity-50"
           >
             {t("main.hero.calls_to_actions.get_in_touch")}
-            <Icons.arrowRight size={16} className="size-4" />
+            <Icons.arrowRight
+              size={16}
+              className="size-4 transition-all duration-200 group-hover:translate-x-1"
+            />
           </a>
         </div>
       </div>
