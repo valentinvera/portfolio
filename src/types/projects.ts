602c9dd56ae8ceb90140c8c4e5b90e845fe43a36
Valentín Vera <valentinvera2805@gmail.com>
2025-02-15 13:06:46 -0300

    feat: add type for projects

---
+++ src/types/projects.ts
@@ -0,0 +1,26 @@
+export type Stack =
+  | "React"
+  | "Tailwind CSS"
+  | "TypeScript"
+  | "Next"
+  | "Nest"
+  | "Shadcn/ui"
+  | "Prisma"
+  | "PostgreSQL"
+  | "Prisma"
+  | "TypeORM"
+  | "MongoDB"
+  | "Redis"
+  | "Docker"
+  | "DrizzleORM"
+  | "Hono"
+  | "Astro"
+
+export interface Projects {
+  imgUrl: string
+  title: string
+  description: string
+  tags: Stack[]
+  url: string
+  repositoryUrl: string
+}
