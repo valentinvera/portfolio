351fba556d6d03b760d9830d15c10297b2dce769
Valentín Vera <valentinvera2805@gmail.com>
2025-02-16 03:50:10 -0300

    feat: add link with content referring to the translation

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -1,4 +1,6 @@
 import { ProjectsData } from "@/data/projects"
+import { Icons } from "@/icons"
+import { Link } from "@tanstack/react-router"
 import { useTranslation } from "react-i18next"
 import ProjectCard from "../project-card"
 
@@ -9,9 +11,21 @@ const Projects = () => {
   return (
     <section className="pt-30 pb-10">
       <div>
-        <h2 className="mb-2 ml:mb-4 font-medium ml:text-3xl text-2xl">
-          {t("main.projects.title")}
-        </h2>
+        <div className="flex items-center justify-between">
+          <h2 className="mb-2 ml:mb-4 font-medium ml:text-3xl text-2xl">
+            {t("main.projects.title")}
+          </h2>
+          <Link
+            to="/projects"
+            className="group flex items-center gap-1 text-muted-foreground transition-colors duration-200 hover:text-foreground"
+          >
+            <span>{t("main.projects.see_more")}</span>
+            <Icons.arrowRight
+              size={16}
+              className="size-4 opacity-70 duration-200 group-hover:translate-x-[2px] group-hover:opacity-100"
+            />
+          </Link>
+        </div>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
           {projects.map(({ imgUrl, title, description, tags, url, repositoryUrl }, index) => (
             <ProjectCard
