b237dce1b46e69997132faf4f029980476699d89
Valentín Vera <valentinvera2805@gmail.com>
2025-02-20 08:27:11 -0300

    feat: add new file with a function to generate a slug and move file to this folder that was previously in another folder

---
+++ src/lib/utils/slug.ts
@@ -0,0 +1,10 @@
+export const slug = (title: string): string => {
+  return title
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "")
+}
