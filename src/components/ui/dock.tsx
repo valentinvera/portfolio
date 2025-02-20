f0994b698a876e29e5555fc00a71fed18c07f14c
Valentín Vera <valentinvera2805@gmail.com>
2025-02-20 07:50:47 -0300

    refactor: update import path

--- src/components/ui/dock.tsx
+++ src/components/ui/dock.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils/cn"
 import { type VariantProps, cva } from "class-variance-authority"
 import { motion, useMotionValue, useSpring, useTransform } from "motion/react"
 import React, { PropsWithChildren, useRef, useEffect, useState } from "react"
