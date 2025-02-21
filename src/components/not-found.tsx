11c20bc163776d48d961dfbfce587bcd7d647ecb
Valentín Vera <valentinvera2805@gmail.com>
2025-02-21 13:28:56 -0300

    feat: add more text with translations and link to go back to home page

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,8 +1,25 @@
+import { Icons } from "@/icons"
+import { Link } from "@tanstack/react-router"
+import { useTranslation } from "react-i18next"
+
 const NotFound = () => {
+  const { t } = useTranslation()
+
   return (
-    <div className="p-4">
-      <h1 className="font-bold text-2xl">404 Not Found</h1>
-      <p>La página que buscas no existe.</p>
+    <div className="flex flex-col items-center justify-center ml:pt-34 pt-22 text-center">
+      <h2 className="font-bold text-9xl">404</h2>
+      <p className="text-lg text-muted-foreground">{t("main.errors.not_found.message")}</p>
+      <p className="mb-4 text-lg">{t("main.errors.not_found.error")}</p>
+      <Link
+        to="/"
+        className="group inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg bg-foreground px-3 py-2 text-background hover:bg-primary/90"
+      >
+        <Icons.arrowRight
+          size={16}
+          className="group-hover:-translate-x-0.5 size-4 rotate-180 transition-transform"
+        />
+        <span>{t("main.errors.not_found.call_to_action")}</span>
+      </Link>
     </div>
   )
 }
