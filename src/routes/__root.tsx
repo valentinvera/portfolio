518ab5c16d363f6c63694240eef672f3425d520c
Valentín Vera <valentinvera2805@gmail.com>
2025-02-21 13:30:54 -0300

    feat: add NotFoundComponent

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,10 @@
 import NavBar from "@/components/navbar"
+import NotFound from "@/components/not-found"
 import { Outlet, createRootRoute } from "@tanstack/react-router"
 
 export const Route = createRootRoute({
   component: RootComponent,
+  notFoundComponent: NotFound,
 })
 
 function RootComponent() {
