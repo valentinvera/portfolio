9c28eab4e1c94b4921a3b5e519d32e360ecec44f
Valentín Vera <valentinvera2805@gmail.com>
2025-02-21 17:00:42 -0300

    refactor: remove code unnecessary

--- src/hooks/useEmail.ts
+++ src/hooks/useEmail.ts
@@ -1,16 +1,8 @@
 import emailjs from "@emailjs/browser"
 
-const getEnvVariable = (key: string) => {
-  if (import.meta.env.MODE === "production") {
-    return import.meta.env[key]
-  } else {
-    return import.meta.env[`VITE_${key}`]
-  }
-}
-
-const SERVICE_ID = getEnvVariable("EMAILJS_SERVICE_ID")
-const TEMPLATE_ID = getEnvVariable("EMAILJS_TEMPLATE_ID")
-const PUBLIC_KEY = getEnvVariable("EMAILJS_PUBLIC_KEY")
+const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID
+const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
+const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
 
 interface EmailParams {
   to_name: string
