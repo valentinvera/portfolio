6ed3f05e23e41e70da5b8944dade62fe6fc952bf
Valentín Vera <valentinvera2805@gmail.com>
2025-03-02 17:55:46 -0300

    feat: add property to tanstack router plugin configuration for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,14 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), TanStackRouterVite({})],
+  plugins: [
+    react(),
+    tailwindcss(),
+    TanStackRouterVite({
+      autoCodeSplitting: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
