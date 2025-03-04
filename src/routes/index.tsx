d9400b609921004dda0fcf1224e4c96bd78d3dd8
Valentín Vera <valentinvera2805@gmail.com>
2025-03-04 17:46:39 -0300

    feat: add use of hook to change page title based on language

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,23 @@ import AboutMe from "@/components/sections/about-me"
 import Contact from "@/components/sections/contact"
 import Hero from "@/components/sections/hero"
 import Projects from "@/components/sections/projects"
+import { useDocumentTitle } from "@/hooks/useDocumentTitle"
 import { createFileRoute } from "@tanstack/react-router"
+import { useTranslation } from "react-i18next"
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 })
 
 function HomeComponent() {
+  const { i18n } = useTranslation()
+
+  useDocumentTitle(
+    i18n.language === "es"
+      ? "Valentín Vera - Desarrollador Front-End"
+      : "Valentín Vera - Front-End Developer",
+  )
+
   return (
     <>
       <Hero />
